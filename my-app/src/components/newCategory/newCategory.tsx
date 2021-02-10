d21ae76a5a67b79114cc1d4719bc6b732f3e7e80
Shira Feldinger <shirafeldinger@gmail.com>
2021-02-10 18:15:23 +0200

    prevent categiry without name

--- my-app/src/components/newCategory/newCategory.tsx
+++ my-app/src/components/newCategory/newCategory.tsx
@@ -7,12 +7,19 @@ const NewCategory = () => {
     const categories = useSelector<MyLocationState>(state => state.categories) as Array<string>;
     const dispatch = useDispatch();
     const [category, setCategory] = useState('')
+    const [formText, setFormText] = useState('')
 
     const addCategory = () => {
         let newCategories = [...categories]
+        if (category.length == 0) {
+            setFormText('Please enter a valid category name')
+            return;
+        }
         newCategories.push(category)
         dispatch({ type: ActionTypes.setCategory, categories: newCategories })
         dispatch({ type: ActionTypes.setCategorySelected, categorySelected: '' })
+        alert('category added')
+
     };
 
 
@@ -20,7 +27,9 @@ const NewCategory = () => {
         <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
             <div className="m-3 form-group">
                 <input placeholder='category name' className="form-control" type="text" onChange={(e) => { setCategory(e.target.value) }} />
-                <Link to="/" className='m-2 btn btn-info' onClick={() => { addCategory() }}>Add</Link>
+                <button className='m-2 btn btn-info' onClick={() => { addCategory() }}>Add</button>
+                <Link className='btn btn-secondary' to='/'>Go Back</Link>
+                <div className='form-text'>{formText}</div>
             </div>
         </div>
     );
